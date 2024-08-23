17b246f24379dd1b986e7c529c7a02e76677beb2
Francina Dippiton <francinagio@hotmail.com>
2024-08-23 14:31:02 -0400

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://fdippiton.github.io/fdippiton-portfolio/",
+  base: "/portafolio/",
   build: {
     outDir: "dist",
   },
